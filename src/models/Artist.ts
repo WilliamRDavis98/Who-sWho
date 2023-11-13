460a32c8a9b6a6aa85163703741f133f3a866cf2
John McConnell <john.mcconnell207@gmail.com>
2023-11-13 17:25:48 -0500

    some ideas and object models

---
+++ src/models/Artist.ts
@@ -0,0 +1,23 @@
+export default interface Artist {
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: {
+    url: string;
+    height: number;
+    width: number;
+  }[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
+
+// maybe for the artists we get, we could use the get related artists so it is more difficult to pick
