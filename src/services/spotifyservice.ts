6bb210851e234870d56837b0dfe0c0944803acc4
John McConnell <john.mcconnell207@gmail.com>
2023-11-14 12:39:36 -0500

    service method notes

---
+++ src/services/spotifyservice.ts
@@ -0,0 +1,27 @@
+
+
+const getRecommendationsUrl = 'https://api.spotify.com/v1/recommendations';
+const getArtistUrl = 'https://api.spotify.com/v1/artists/';
+// add ons to the getArtistUrl after the Id
+// get top tracks: '/top-tracks'
+// get related artists: '/related-artists'
+const getTrackUrl = 'https://api.spotify.com/v1/tracks/';
+
+
+export class spotifyService {
+
+
+// method: getRecommendations
+// needs to get the genres from user selection
+// needs to use the token in the call
+// maps to array of artistIds
+
+// method: chooseRandomArtist
+// loops through array of artistIds and then selects a random artist
+// helper method potentially
+
+// method: getArtistTopTracks
+
+//
+
+}
