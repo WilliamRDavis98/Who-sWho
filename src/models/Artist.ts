1425d5069928021e274669ce4321b6bc9ac4892b
John McConnell <john.mcconnell207@gmail.com>
2023-11-14 11:24:52 -0500

    additional api call ideas

--- src/models/Artist.ts
+++ src/models/Artist.ts
@@ -23,3 +23,7 @@ export default interface Artist {
 // maybe for the artists we get, we could use the get related artists so it is more difficult to pick
 // maybe get random artists and if the genres property contains the selected genre then add
 // could also set an ID for an artist within each genre, and then get related artists, and then choose random
+// could also do a get reccomendations call, which incorporates the genres and gets an amount of tracks that are reccomended,
+// then it loops through the sets of tracks, gets the artist ids and creates a an array of artist ids
+// then selects one as random, and gets related artists for the score
+// this way the user can test multiple genres
--- src/services/getArtistAPI.ts
+++ src/services/getArtistAPI.ts
@@ -5,5 +5,6 @@ const artistAPI = "https://api.spotify.com/v1/artists";
 export class getArtistAPI {
 
   private accessToken =
+  // token changes every hour
 
 }
