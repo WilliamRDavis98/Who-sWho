8b3337481c3531c4d8380dd2b14d3ac914883f25
William Davis <williamrdavis98@gmail.com>
2023-11-14 17:27:33 -0500

    Music working

--- src/app/guess/guess.component.ts
+++ src/app/guess/guess.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SongService } from '../song.service';
 import { Router } from '@angular/router';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-guess',
@@ -15,6 +16,11 @@ export class GuessComponent implements OnInit {
   songNumber: number = 1
   artistNumber: number = 2
   artistId: string = ''
+  song1 = new Howl({
+    src: ['https://p.scdn.co/mp3-preview/c590292029e985515f7063e8d5d291d677694eb9?cid=c4199e9be8874c78b1199eea6593dad4'],
+    html5 :true,
+    volume: .5
+  });
 
   ngOnInit(): void {
     this.songData.currentGenre.subscribe(currentGenre => this.genre = currentGenre)
@@ -25,6 +31,24 @@ export class GuessComponent implements OnInit {
 
   onSubmit(){
     this.router.navigateByUrl('/')
+  }
+  
+  toggleSong(currentSong: Howl){
+    if(currentSong.playing()){
+      currentSong.pause()      
+    }else{
+      currentSong.play()
+    }
+  }
+
+  incrementVolume(currentSong: Howl){
+    currentSong.volume(currentSong.volume() + .1)
+    console.log(currentSong.volume())
+  }
+
+  decrementVolume(currentSong: Howl){
+    currentSong.volume(currentSong.volume() - .1)
+    console.log(currentSong.volume())
   }
 
 }
